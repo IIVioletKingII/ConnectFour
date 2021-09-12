a6a3543b86dc4a3bd6e30a4889ed1ad9e92cbbee
Sam DePoule <samdepoule@gmail.com>
2021-09-12 13:04:44 -0700

    Neatening

--- app/src/main/java/com/example/connectfour/GameActivity.java
+++ app/src/main/java/com/example/connectfour/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.connectfour;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -8,10 +9,12 @@ import androidx.core.graphics.drawable.DrawableCompat;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.GridLayout;
 
 import android.view.View;
@@ -31,6 +34,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 	public static int border = 2;
 
 	public int squareSize = 0;
+
+	private ImageView[][] pieceGrid;
+
+	private boolean playerOneTurn;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -53,8 +60,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 		initToolbar( );
 
 		TextView title = findViewById( R.id.gameTitle );
-		String titleText = "" + connectFourGame.getPanelHeight( ) + "*" + connectFourGame.getPanelWidth( ) + " Connect Game";
+		String titleText = "" + panelHeight + "*" + panelWidth + " Connect Game";
 		title.setText( titleText );
+
+		pieceGrid = new ImageView[ /*rows*/ panelHeight][ /*columns*/ panelWidth];
 
 		AtomicInteger toastShowTimes = new AtomicInteger( );
 
@@ -69,8 +78,62 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 				connectFourGame.startTimer( title );
 		} );
 
+		new Thread( ( ) -> {
+
+			while( pieceGrid[4][3] == null ) ;
+
+			ImageView examplePos = pieceGrid[1][3];
+			Drawable buttonDrawable = examplePos.getBackground( );
+			buttonDrawable = DrawableCompat.wrap( buttonDrawable );
+			// the color is a direct color int and not a color resource
+			DrawableCompat.setTint( buttonDrawable, Color.RED );
+			examplePos.setBackground( buttonDrawable );
+		} )/*.start( )*/;
+
 	}
 
+	@RequiresApi(api = Build.VERSION_CODES.O)
+	public void gridPosClicked( View view ) {
+
+		String toolTipText = view.getTooltipText( ).toString( );
+
+		// set bottom of that column to be person's turn
+
+		ImageView currentPosition = (ImageView) view;
+
+		if( currentPosition != null ) {
+
+			int x = Integer.parseInt( toolTipText.substring( 0, 1 ) );
+			int y = Integer.parseInt( toolTipText.substring( 2, 3 ) );
+
+//			runTransitionToPos( x, y, playerOneTurn );
+
+			setColor( currentPosition, /*red's turn ? */Color.RED /* : Color.YELLOW*/ );
+//			new Thread( () -> setColor( currentPosition, /*red's turn ? */Color.RED /* : Color.YELLOW*/ ) ).start( );
+			Toast.makeText( this, "current: " + toolTipText, Toast.LENGTH_SHORT ).show( );
+		}
+	}
+
+	public static void setColor( View view, int color ) {
+
+		Drawable buttonDrawable = view.getBackground( );
+		buttonDrawable = DrawableCompat.wrap( buttonDrawable );
+		// the color is a direct color int and not a color resource
+		DrawableCompat.setTint( buttonDrawable, color );
+		view.setBackground( buttonDrawable );
+	}
+
+	private int[] getGridPos( int x, int y ) {
+		return new int[]{ 0, 0 };
+	}
+
+	private void runTransitionToPos( int x, int y, boolean playerOne ) {
+		ImageView hiddenImage = pieceGrid[x][y];
+
+
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.O)
 	private void initSquares( ) {
 
 		int panelHeight = connectFourGame.getPanelHeight( );
@@ -102,7 +165,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 			for( int width = 0; width < panelWidth; width++ ) {
 
 				ImageView curGridPos = new ImageView( this );
-				String gridPosText = "" + height + "-" + width;
+				String gridPosText = height + "_" + width;
+				curGridPos.setTooltipText( gridPosText );
+
+				curGridPos.setOnClickListener( ( v ) -> {
+					Log.e( "ONCLICK", gridPosText + ":clicked: " + curGridPos.getTooltipText( ) );
+					gridPosClicked( v );
+				} ); // gridPosClicked
 
 				curGridPos.setBackgroundResource( R.drawable.piece );
 				Drawable buttonDrawable = curGridPos.getBackground( );
@@ -117,11 +186,11 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 				curGridPos.setMinimumHeight( squareSize );
 				curGridPos.setMaxHeight( squareSize );
 
+				pieceGrid[height][width] = curGridPos;
+
 				new Handler( Looper.getMainLooper( ) ).post( ( ) -> gridLayout.addView( curGridPos ) );
 			}
 		}
-
-//		new Handler( Looper.getMainLooper( ) ).post( ( ) -> gridLayout.set );
 	}
 
 	private void initToolbar( ) {
